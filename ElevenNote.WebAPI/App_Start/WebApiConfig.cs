cba03baf7378ba7ad8dc0be7c23a5e1afcd17caf
Caleb Addie <cjaddie.89@gmail.com>
2021-01-31 20:03:36 -0500

    DateTimeOffset View Added

--- ElevenNote.WebAPI/App_Start/WebApiConfig.cs
+++ ElevenNote.WebAPI/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace ElevenNote.WebAPI
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
